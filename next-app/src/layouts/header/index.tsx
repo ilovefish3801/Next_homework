0ff034f4ab06e1b5ec58e8dbc66df2ca90a4444d
kurulo kezemiakin <pipipupi.kom@gmail.com>
2023-11-19 15:01:52 +0200

    a

--- next-app/src/layouts/header/index.tsx
+++ next-app/src/layouts/header/index.tsx
@@ -3,9 +3,10 @@ import Link from 'next/link'
 // image tag
 import Image from 'next/image'
 // images to use
-import input_img from "@/assets/images/Search.jpg"
+import input_img from "@/assets/images/Search.png"
 import liked_img from "@/assets/images/Heart.jpg"
 import cart_img from "@/assets/images/Buy.jpg"
+import downArrow_img from "@/assets/images/Arrow_Down.jpg"
 // styles
 import s from './header.module.scss'
 import font from "@/styles/fonts.module.css"
@@ -14,41 +15,47 @@ import font from "@/styles/fonts.module.css"
 const header = () => {
   return (
     <>
-        <div className={s.container}>
+        <header className={s.header}>
+          <div className={s.container}>
             
-        <div className={`${s.container__logo} ${font.inter}`}>Logo</div>
+            <div className={`${s.container__logo} ${font.inter}`}>Logo</div>
             
             <form action="" className={s.container__form}>
-
+                
                 <input type="text" className={s.container__form_input}/>
                 <Image src={input_img} alt="" quality={100} className={s.container__form_image}></Image>
             
             </form>
-
-        </div>
-
-        <ul>
-          <li>
-            <Link href={'#'}>Товари</Link>
-          </li>
-          <li>
-            <Link href={'#'}>Про нас</Link>
-          </li>
-          <li>
-            <Link href={'#'}>Контакти</Link>
-          </li>
-          <li>
-            <Link href={'#'}>UA</Link>
-          </li>
-          <li>
-            <Image src={liked_img} alt=''></Image>
-          </li>
-          <li>
-            <Image src={cart_img} alt=''></Image>
-          </li>
-        </ul>
-
-
+  
+          </div>
+  
+          <nav className={`${font.inter} ${s.navigation}`}>
+            <ul>
+              <li>
+                <Link href={'#'}>Товари</Link>
+                <Image src={downArrow_img} alt=''></Image>
+              </li>
+              <li>
+                <Link href={'#'}>Про нас</Link>
+              </li>
+              <li>
+                <Link href={'#'}>Контакти</Link>
+              </li>
+              <li>
+                <Link href={'#'}>UA</Link>
+                <Image src={downArrow_img} alt=''></Image>
+              </li>
+              <li>
+                <Image src={liked_img} alt=''></Image>
+              </li>
+              <li>
+                <Image src={cart_img} alt=''></Image>
+              </li>
+            </ul>
+          </nav>
+  
+          <button className={`${font.inter} ${s.loginBtn}`}>Вхід</button>
+        </header>
     </>
   )
 }
