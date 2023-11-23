097e65b542c626132d1d84724305b6b29e48bc15
kurulo kezemiakin <pipipupi.kom@gmail.com>
2023-11-23 17:23:42 +0200

    a

--- next-app/src/layouts/header/index.tsx
+++ next-app/src/layouts/header/index.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {use, useState} from 'react'
 import Link from 'next/link'
 // image tag
 import Image from 'next/image'
@@ -7,14 +7,18 @@ import input_img from "@/assets/images/Search.png"
 import liked_img from "@/assets/images/Heart.jpg"
 import cart_img from "@/assets/images/Buy.jpg"
 import downArrow_img from "@/assets/images/Arrow_Down.jpg"
+import upArrow_img from "@/assets/images/Arrow_Up.jpg"
 // styles
 import s from './header.module.scss'
 import font from "@/styles/fonts.module.css"
 
 
 const header = () => {
-  // Hiding search image when person focuses on input
-  let isInputFocused = false
+  // states
+  const [focus, setFocus] = useState<boolean>(false)
+  const [productsHover, setHoverProducts] = useState<boolean>(false)
+  const [langHover, setHoverLang] = useState<boolean>(false)
+  const [burger, setBurger] = useState<boolean>(false)
 
   return (
     <>
@@ -24,29 +28,43 @@ const header = () => {
           
           <form action="" className={s.form}>
               
-              <input onBlur={()=>{isInputFocused = false}} onFocus={()=>{isInputFocused = true}} type="text" className={`${s.form__input} ${font.inter}`}/>
-              <Image src={input_img} alt="" quality={100} className={`${s.form__image} ${isInputFocused ? "hide" : "show"}`}></Image>
+              <input onBlur={()=>{setFocus(false)}} onFocus={()=>{setFocus(true)}} type="text" className={`${s.form__input} ${font.inter}`}/>
+              <Image src={input_img} alt="" quality={100} className={`${s.form__image} ${focus ? s.hide : s.show}`}></Image>
           
           </form>
   
   
           <nav className={`${font.inter} ${s.navigation}`}>
             <ul>
-              <li>
+              <li style={{position: "relative"}}>
                 <Link href={'#'}>Товари</Link>
-                <Image src={downArrow_img} alt=''></Image>
+                <Image onClick={()=>{productsHover ? setHoverProducts(false) : setHoverProducts(true)}} src={productsHover ? upArrow_img : downArrow_img} alt=''></Image>
+                <div style={productsHover ? {display: "block"} : {display: "none"}} className={`${s.dropdownProducts} ${s.dropdown}`}>Я не знав що сюди додати</div>
               </li>
+              
               <li>
                 <Link href={'#'}>Про нас</Link>
               </li>
+              
               <li>
                 <Link href={'#'}>Контакти</Link>
               </li>
-              <li>
-                <Link href={'#'}>UA</Link>
-                <Image src={downArrow_img} alt=''></Image>
+              
+              <li style={{position: "relative"}}>
+                <div>UA</div>
+                <Image onClick={()=>{langHover ? setHoverLang(false) : setHoverLang(true)}} src={langHover ? upArrow_img : downArrow_img} alt=''></Image>
+                <div style={langHover ? {display: "block"} : {display: "none"}} className={`${s.dropdownLang} ${s.dropdown}`}>Я не знав що сюди додати</div>
               </li>
             </ul>
+
+            <div onClick={()=>{
+              setBurger(true)
+            }} className={`${s.navigation__burger} ${burger ? s.active : s.unactive}}`}>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+
           </nav>
 
           <div className={s.interactionBtns}>
